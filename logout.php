e9aac3b0aefb88f1f9ecaeffd68fb4b4e26516c0
hatimSa <hatimsalhi03@gmail.com>
2023-12-17 18:29:41 +0100

    up

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+// Supprimer toutes les variables de session
+$_SESSION = array();
+
+// Détruire complètement la session
+session_destroy();
+
+// Rediriger vers la page de connexion
+header("Location: login.html");
+exit;
